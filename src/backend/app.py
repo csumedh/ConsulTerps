fe181761fe43c97b74ca2b571c9aeb61aec4bb20
csumedh <sumedhc@umd.edu>
2025-05-06 23:40:45 -0400

    -changes to deployment

--- src/backend/app.py
+++ src/backend/app.py
@@ -113,3 +113,7 @@ def not_found(e):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
